bb6e57f423ea2010639c19f8ec11cb1800a361d7
yossef-khaled <yossef.k.y333@gmail.com>
2022-02-26 17:34:35 +0200

    Create context for language

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 import Home from './Components/Home/Home';
+import { languageContext } from './LanguageContext';
 
 function App() {
+
+  const [language, setLanguage] = useState('ar'); 
+  
   return (
     <div className="App">
-      <Home/>
+      <languageContext.Provider value={[language, setLanguage]}>
+        <Home/>
+      </languageContext.Provider> 
     </div>
   );
 }
