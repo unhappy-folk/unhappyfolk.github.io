d0a7cd766ac349a64058ae015232315307b64756
yossef-khaled <yossef.k.y333@gmail.com>
2022-03-14 12:40:34 +0200

    TODO function to display the quotes randomly

--- src/Components/Quote/Quote.jsx
+++ src/Components/Quote/Quote.jsx
@@ -1,7 +1,17 @@
 import QUOTES_SCHEMA from '../../Content/Quotes/Schema'; 
 import LanguagesBar from '../LanguagesBar/LanguagesBar';
+import { useEffect, useContext, useState } from 'react';
+import { languageContext } from "../../LanguageContext";
 
 function Quote() {
+
+    const [currentLanguage, setCurrentLanguage] = useContext(languageContext);
+    const [currentQuote, setCurrentQuote] = useState({});
+
+    useEffect(() => {
+        //TODO....
+    }, [currentLanguage, 6000])
+
     return (
         <div className="quote-container">
             <LanguagesBar/>
