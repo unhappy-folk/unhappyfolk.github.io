809b24ed4811fdfb524f9e3be84f2329fadd47ba
yossef-khaled <yossef.k.y333@gmail.com>
2022-04-09 02:48:55 +0200

    Use quote context to set language quote is written in.

--- src/Components/Quote/Quote.jsx
+++ src/Components/Quote/Quote.jsx
@@ -8,7 +8,8 @@ import { MESSAGES } from '../../Content/Quotes/Schema';
 import LanguagesBar from '../LanguagesBar/LanguagesBar';
 
 //Import context
-import { LanguageContext } from "../../LanguageContext";
+import { LanguageContext } from "../../Context";
+import { QuoteContext } from '../../Context';
 
 //Import style
 import './Quote.css'
@@ -16,7 +17,7 @@ import './Quote.css'
 function Quote() {
 
     const [currentLanguage, setCurrentLanguage] = useContext(LanguageContext);
-    const [currentQuote, setCurrentQuote] = useState(MESSAGES[0]);
+    const [currentQuote, setCurrentQuote] = useContext(QuoteContext);
     const [fade, setFade] = useState('fade-in');
     const [isShown, setIsShown] = useState(true);
 
@@ -29,29 +30,23 @@ function Quote() {
     }, [isShown])
 
     useEffect(() => {
-        console.log(currentQuote);
-    }, [currentQuote])
-
-    useEffect(() => {
         let showQuoteInterval = setInterval(() => {
             setFade('fade-out');
             setIsShown(false);
         }, 14000);
-    }, [])
-
-    
+    }, []);   
 
     return (
-        
-        <div className={`quote-container`}>
-            <p style={{fontSize: '150%'}}></p>
-            <LanguagesBar/> 
-            {currentQuote.text ? 
-            <div className={`${fade}`}>
-                <p className="quote">{currentLanguage == 'ar' ? currentQuote.text.ar : currentQuote.text.en}</p>
-                <a className="quote-ref" href={currentQuote.src}>{currentLanguage == 'ar' ? currentQuote.author.ar : currentQuote.author.en}</a>
+        <div className='container'>
+            <LanguagesBar/>
+            <div className={`quote-container`}>
+                {currentQuote.text ? 
+                <div className={`${fade}`}>
+                    <p className="quote">{currentQuote.text[`${currentLanguage}`]}</p>
+                    <a className="quote-ref" href={currentQuote.src}>{currentQuote.author[`${currentLanguage}`]}</a>
+                </div>
+                : ''}
             </div>
-            : ''}
         </div>
     )
 }
