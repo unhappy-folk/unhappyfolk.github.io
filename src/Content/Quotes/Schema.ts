2809fe0161320496292aba2f809e9508efba4e48
Muhammad Hashim <msg@mhashim6.me>
2022-05-18 15:42:20 +0200

    Yusuf Ali + ref touches

--- src/Content/Quotes/Schema.ts
+++ src/Content/Quotes/Schema.ts
@@ -26,14 +26,14 @@ export const MESSAGES: Array<Message> = [
   },
   {
     type: "Quran",
-    src: "https://quran.com/2:156/tafsirs/en-tafsir-maarif-ul-quran",
+    src: "https://quran.com/2/156",
     author: {
       ar: "سورة البقرة - آية رقم ١٥٦",
-      en: "Surah AL-Baqarah - verse 156",
+      en: "Chapter 2 (The Cow), 156",
     },
     text: {
       ar: "الَّذِينَ إِذَآ أَصَـٰبَتْهُم مُّصِيبَةٌ قَالُوٓا إِنَّا لِلَّهِ وَإِنَّآ إِلَيْهِ رَٰجِعُونَ",
-      en: "Who, when disaster strikes them, say, Indeed we belong to Allāh, and indeed to Him we will return",
+      en: 'Who say, when afflicted with calamity: "To Allah We belong, and to Him is our return"',
     },
   },
   {
@@ -41,7 +41,7 @@ export const MESSAGES: Array<Message> = [
     src: "https://sunnah.com/bukhari:6351",
     author: {
       ar: "صحيح البخاري -	٦٣٥١",
-      en: "Sahih al-bukhari - 6351",
+      en: "Prophet Muhammad (ﷺ)",
     },
     text: {
       ar: "لاَ يَتَمَنَّيَنَّ أَحَدٌ مِنْكُمُ الْمَوْتَ لِضُرٍّ نَزَلَ بِهِ، فَإِنْ كَانَ لاَ بُدَّ مُتَمَنِّيًا لِلْمَوْتِ فَلْيَقُلِ اللَّهُمَّ أَحْيِنِي مَا كَانَتِ الْحَيَاةُ خَيْرًا لِي، وَتَوَفَّنِي إِذَا كَانَتِ الْوَفَاةُ خَيْرًا لِي",
@@ -53,7 +53,7 @@ export const MESSAGES: Array<Message> = [
     src: "https://sunnah.com/riyadussalihin:469",
     author: {
       ar: "صحيح مسلم - ٢٩٥٦",
-      en: "Sahih Muslim - 2956",
+      en: "Prophet Muhammad (ﷺ)",
     },
     text: {
       ar: "الدُّنْيَا سِجْنُ الْمُؤْمِنِ وَجَنَّةُ الْكَافِرِ",
@@ -65,7 +65,7 @@ export const MESSAGES: Array<Message> = [
     src: "https://islamic-content.com/hadeeth/2012/en",
     author: {
       ar: "(٦/٤) مسند أحمد - ٢٣٨٦٥",
-      en: "Musnad Ahmed - 23865 (6/4)",
+      en: "Prophet Muhammad (ﷺ)",
     },
     text: {
       ar: "ليَبْلغنَّ هذا الأمرُ ما بلغ الليلُ والنهارُ، ولا يترك اللهُ بيت مَدَر ولا وَبَر إلا أدخله الله هذا الدين، بعِزِّ عزيز أو بذُلِّ ذليل، عزا يُعِزُّ الله به الإسلام، وذُلا يُذل الله به الكفر",
@@ -81,7 +81,7 @@ export const MESSAGES: Array<Message> = [
     },
     text: {
       ar: "وَزُخْرُفًۭا ۚ وَإِن كُلُّ ذَٰلِكَ لَمَّا مَتَـٰعُ ٱلْحَيَوٰةِ ٱلدُّنْيَا ۚ وَٱلْـَٔاخِرَةُ عِندَ رَبِّكَ لِلْمُتَّقِينَ",
-      en: "and ornaments ˹of gold˺. Yet all this is no more than a ˹fleeting˺ enjoyment in this worldly life. ˹But˺ the Hereafter with your Lord is ˹only˺ for those mindful ˹of Him˺.",
+      en: "And also adornments of gold. But all this were nothing but conveniences of the present life: The Hereafter, in the sight of thy Lord is for the Righteous.",
     },
   },
   {
@@ -92,8 +92,8 @@ export const MESSAGES: Array<Message> = [
       en: "Surah AL-A'la",
     },
     text: {
-      ar: "﴾بَلْ تُؤْثِرُونَ ٱلْحَيَوٰةَ ٱلدُّنْيَا﴿١٦﴾ وَٱلْـَٔاخِرَةُ خَيْرٌۭ وَأَبْقَىٰٓ﴿١٧",
-      en: "But you ˹deniers only˺ prefer the life of this world,﴾16﴿ even though the Hereafter is far better and more lasting.﴾17﴿ ",
+      ar: "﴾بَلْ تُؤْثِرُونَ ٱلْحَيَوٰةَ ٱلدُّنْيَا ﴿١٦﴾ وَٱلْـَٔاخِرَةُ خَيْرٌۭ وَأَبْقَىٰٓ ﴿١٧",
+      en: "Nay (behold), ye prefer the life of this world; But the Hereafter is better and more enduring.",
     },
   },
   {
@@ -101,11 +101,11 @@ export const MESSAGES: Array<Message> = [
     src: "https://quran.com/24/14",
     author: {
       ar: "سورة النور - آية ١٤",
-      en: "Surah AL-Nur - verse 14",
+      en: "Chapter 24 (The Light), 14",
     },
     text: {
       ar: "وَلَوْلَا فَضْلُ ٱللَّهِ عَلَيْكُمْ وَرَحْمَتُهُۥ فِى ٱلدُّنْيَا وَٱلْـَٔاخِرَةِ لَمَسَّكُمْ فِى مَآ أَفَضْتُمْ فِيهِ عَذَابٌ عَظِيمٌ",
-      en: "Had it not been for Allah’s grace and mercy upon you in this world and the Hereafter, you would have certainly been touched with a tremendous punishment for what you plunged into—",
+      en: "Were it not for the grace and mercy of Allah on you, in this world and the Hereafter, a grievous penalty would have seized you in that ye rushed glibly into this affair.",
     },
   },
   {
@@ -113,11 +113,11 @@ export const MESSAGES: Array<Message> = [
     src: "https://quran.com/2/253",
     author: {
       ar: "سورة البقرة - آية ٢٥٣",
-      en: "Surah AL-Baqarah - verse 253",
+      en: "Chapter 2 (The Cow), verse 253",
     },
     text: {
-      ar: "وَلَـٰكِنَّ ٱللَّهَ يَفْعَلُ مَا يُرِيدُ",
-      en: "But Allah does what He wills.",
+      ar: "وَلَـٰكِنَّ ٱللَّهَ يَفْعَلُ مَا يُرِيدُ...",
+      en: "....but Allah Fulfilleth His plan.",
     },
   },
   {
@@ -129,7 +129,7 @@ export const MESSAGES: Array<Message> = [
     },
     text: {
       ar: "كَذَٰلِكَ ٱللَّهُ يَفْعَلُ مَا يَشَآءُ",
-      en: "So will it be. Allah does what He wills.",
+      en: "Doth Allah accomplish what He willeth.",
     },
   },
   {
@@ -137,11 +137,11 @@ export const MESSAGES: Array<Message> = [
     src: "https://quran.com/2/26",
     author: {
       ar: "سورة البقرة - آية ٢٦",
-      en: "Surah Al-Baqarah - verse 26",
+      en: "Chapter 2 (The Cow), 26",
     },
     text: {
       ar: "إِنَّ ٱللَّهَ لَا يَسْتَحْىِۦٓ أَن يَضْرِبَ مَثَلًۭا مَّا بَعُوضَةًۭ فَمَا فَوْقَهَا ۚ فَأَمَّا ٱلَّذِينَ ءَامَنُوا۟ فَيَعْلَمُونَ أَنَّهُ ٱلْحَقُّ مِن رَّبِّهِمْ ۖ وَأَمَّا ٱلَّذِينَ كَفَرُوا۟ فَيَقُولُونَ مَاذَآ أَرَادَ ٱللَّهُ بِهَـٰذَا مَثَلًۭا ۘ يُضِلُّ بِهِۦ كَثِيرًۭا وَيَهْدِى بِهِۦ كَثِيرًۭا ۚ وَمَا يُضِلُّ بِهِۦٓ إِلَّا ٱلْفَـٰسِقِينَ",
-      en: "Surely Allah does not shy away from using the parable of a mosquito or what is even smaller. As for the believers, they know that it is the truth from their Lord. And as for the disbelievers, they argue, “What does Allah mean by such a parable?” Through this ˹test˺, He leaves many to stray, and guides many. And He leaves none to stray except the rebellious—",
+      en: 'Allah disdains not to use the similitude of things, lowest as well as highest. Those who believe know that it is truth from their Lord; but those who reject Faith say: "What means Allah by this similitude?" By it He causes many to stray, and many He leads into the right path; but He causes not to stray, except those who forsake (the path)',
     },
   },
   {
@@ -153,7 +153,7 @@ export const MESSAGES: Array<Message> = [
     },
     text: {
       ar: "وَمَا ٱلْحَيَوٰةُ ٱلدُّنْيَآ إِلَّا لَعِبٌۭ وَلَهْوٌۭ ۖ وَلَلدَّارُ ٱلْـَٔاخِرَةُ خَيْرٌۭ لِّلَّذِينَ يَتَّقُونَ ۗ أَفَلَا تَعْقِلُونَ",
-      en: "This worldly life is no more than play and amusement, but far better is the ˹eternal˺ Home of the Hereafter for those mindful ˹of Allah˺. Will you not then understand?",
+      en: "What is the life of this world but play and amusement? But best is the home in the hereafter, for those who are righteous. Will ye not then understand?",
     },
   },
   {
@@ -165,7 +165,7 @@ export const MESSAGES: Array<Message> = [
     },
     text: {
       ar: "يَـٰقَوْمِ إِنَّمَا هَـٰذِهِ ٱلْحَيَوٰةُ ٱلدُّنْيَا مَتَـٰعٌۭ وَإِنَّ ٱلْـَٔاخِرَةَ هِىَ دَارُ ٱلْقَرَارِ",
-      en: "O  my people! This worldly life is only ˹a fleeting˺ enjoyment, whereas the Hereafter is truly the home of settlement.",
+      en: "O my people! This worldly life is only ˹a fleeting˺ enjoyment, whereas the Hereafter is truly the home of settlement.",
     },
   },
   {
@@ -173,11 +173,11 @@ export const MESSAGES: Array<Message> = [
     src: "https://quran.com/2/282",
     author: {
       ar: "سورة البقرة - أية ٢٨٢",
-      en: "Surah AL-Baqarah - verse 282",
+      en: "Chapter 2 (The Cow), 282",
     },
     text: {
-      ar: "ۚ وَلَا يَأْبَ كَاتِبٌ أَن يَكْتُبَ كَمَا عَلَّمَهُ ٱللَّهُ",
-      en: "The scribe should not refuse to write as Allah has taught them to write.",
+      ar: "ۚ وَلَا يَأْبَ كَاتِبٌ أَن يَكْتُبَ كَمَا عَلَّمَهُ ٱللَّهُ...",
+      en: "...Let not the scribe refuse to write: as Allah Has taught him.",
     },
   },
   {
