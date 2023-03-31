ea84653a9827aeb4b8742ddaba5cba29694931cf
Yousef Khaled <yousef.khaled@stellastays.com>
2023-03-31 15:17:53 +0200

    Add view website & download buttons in project preview

--- src/Components/ProjectPreview/ProjectPreview.jsx
+++ src/Components/ProjectPreview/ProjectPreview.jsx
@@ -6,14 +6,32 @@ function ProjectPreview ({project}) {
     const [currentLanguage, setCurrentLanguage] = useContext(LanguageContext);
     
     return (
-        <div className="h-24 ease-out duration-300 hover:h-48 m-2 p-2 rounded-xl shadow-md overflow-hidden bg-black text-[#facc15] w-100 lg:w-2/3" dir={`${currentLanguage === 'ar' ? 'rtl' : 'ltr'}`}>
-            <div className="flex justify-center bg-red">
+        <div className="h-24 hover:h-48 overflow-y-auto overscroll-contain  ease-out duration-300 m-2 p-2 rounded-xl shadow-md overflow-hidden bg-black text-[#facc15] w-100 lg:w-2/3" dir={`${currentLanguage === 'ar' ? 'rtl' : 'ltr'}`}>
+            <div className="flex justify-center items-center">
                 {project.logo && 
-                <img src={project.logo} alt={`${project.name} logo`} className="self-center h-14 object-scale-down"/>
+                <div className="w-14">
+                    <img src={project.logo} alt={`${project.name} logo`} className="self-center h-14 object-scale-down"/>
+                </div>
                 }
-                <p className="m-2 md:text-xl text-md">{project.name.toUpperCase()}</p>
+                <p className="m-1 md:text-xl text-md">{project.name.toUpperCase()}</p>
             </div>
             <p className="text-zinc-50 text-clip">{project.about[currentLanguage]}</p>
+            <div className="flex justify-center center-items">
+                { project.visit &&
+                <a href={project.visit}>
+                    <button className="hover:bg-black hover:text-[#facc15] border-2 border-[#facc15] transition ease-in-out duration-300 m-1 sm:m-5 rounded-md py-2 px-4 bg-[#facc15] text-black">
+                        Visit website
+                    </button>
+                </a>
+                }
+                { project.download &&
+                    <a href={project.download}>
+                        <button className="hover:bg-black hover:text-[#facc15] border-2 border-[#facc15] transition ease-in-out duration-300 m-1 sm:m-5 rounded-md py-2 px-4 bg-[#facc15] text-black">
+                            Download
+                        </button>
+                    </a>
+                }
+            </div>
         </div>
     )
 }
