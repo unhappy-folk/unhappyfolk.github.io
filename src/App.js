89a4b9dee76e35ae824402974c96c1a941898938
Muhammad Hashim <msg@mhashim6.me>
2023-04-01 05:56:44 +0200

    `t` function for translation
    Signed-off-by: Muhammad Hashim <msg@mhashim6.me>

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import { LanguageContext } from "./Context";
 import { QuoteContext } from "./Context";
 import { MESSAGES } from "./Content/Quotes/Schema";
 import Footer from "./Components/Footer/Footer";
+import { translate } from "./Content/Projects";
 
 function App() {
   const [language, setLanguage] = useState("ar");
   const direction = language === "ar" ? "rtl" : "ltr";
   const [quote, setQuote] = useState(MESSAGES[1]);
+  const t = (text) => translate(text, language);
 
   return (
-    <LanguageContext.Provider value={[language, direction, setLanguage]}>
+    <LanguageContext.Provider value={[t, language, direction, setLanguage]}>
       <QuoteContext.Provider value={[quote, setQuote]}>
         <Home />
         <br />
