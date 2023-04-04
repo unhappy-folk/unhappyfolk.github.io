fd03f7d5d2f8cd5f48a3de7d37560c24660c686a
Muhammad Hashim <msg@mhashim6.me>
2023-04-05 01:30:36 +0200

    rm types.ts
    Signed-off-by: Muhammad Hashim <msg@mhashim6.me>

--- src/utils/DataFetchers.ts
+++ src/utils/DataFetchers.ts
@@ -1,21 +1,24 @@
 //Import from octo
 import { Octokit } from "octokit";
+import { Organisation } from "../Content/Projects/model";
 
 //Import types
-import { Organization } from "../types";
 
-export async function getTopProjects (org: Organization) {
-    
-    const octokit = new Octokit({
-        auth: process.env.ACCESS_TOKEN
-    });
-      
-    const topProjects = await octokit.request('GET /orgs/{org}/repos?per_page=3', {
+export async function getTopProjects(org: Organisation) {
+  const octokit = new Octokit({
+    auth: process.env.ACCESS_TOKEN,
+  });
+
+  const topProjects = await octokit
+    .request("GET /orgs/{org}/repos?per_page=3", {
       org: org.name,
-    //   headers: {
-    //     'X-GitHub-Api-Version': '2022-11-28',
-    //   }
-    }).then((res) => {return res.data})
+      //   headers: {
+      //     'X-GitHub-Api-Version': '2022-11-28',
+      //   }
+    })
+    .then((res) => {
+      return res.data;
+    });
 
-    return topProjects;
+  return topProjects;
 }
